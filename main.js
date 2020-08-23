aa930966b1d2e1ca17fb4cb1837fb40f79d6a9ee
enddk <69759451+enddk@users.noreply.github.com>
2020-08-24 01:04:29 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(function(){
   
   $('.nav-link').click(function(){
     var id = $(this).attr('href');
-    var position = $(id).offset().top;
+    var position = $(id).offset().top - 50;
     $('html,body').animate({
       'scrollTop':position 
     }, 500);
